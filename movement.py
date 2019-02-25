5affcf76362005554cd96751f0ddb3312e3e5eec
Szymon Pluta <satanthesavior>
2019-02-25 14:20:10 +0100

    Movement alpha version

--- movement.py
+++ movement.py
@@ -21,3 +21,24 @@ def get_char_in_terminal():
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return char
+
+
+def move_hero():
+    maps = data_manager.get_maps_from_file("maps/level1.txt")
+    HERO_POS = [1, 20]
+    maps[HERO_POS[1]][HERO_POS[0]] = "@"
+    for line in maps:
+        print("".join(line))
+    if get_char_in_terminal() == "w":
+        os.system("clear")
+        HERO_POS[0] += 1
+        HERO_POS[1] += 1
+    print(HERO_POS[0], HERO_POS[1])
+    maps[HERO_POS[1]][HERO_POS[0]] = "@"
+    for line in maps:
+        print("".join(line))
+
+    
+
+
+move_hero()
